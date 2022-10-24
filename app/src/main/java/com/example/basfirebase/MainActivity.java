9ba6b53a001c389ebfc206278af664454564111d
bastiChubi <bastian.rojas51@inacapmail.cl>
2022-10-24 12:43:30 -0300

    clase 24-10-22

--- app/src/main/java/com/example/basfirebase/MainActivity.java
+++ app/src/main/java/com/example/basfirebase/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.basfirebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    public void llamadoBtnRegistrar(){
+        btnRegistrar = (Button) findViewById(R.id.btnRegistrar);
+        btnRegistrar.setOnClickListener(new. View.OnClickListener() {
+            @Override
+             public void OnClick(View view) {
+                String nombre = etNombre.getText().toString();
+                String apellido = etApellido.get.Text().toString();
+                int edad = Integer.parseInt(etEdad.getText().toString());
+                String key = UUID.randomUUID().toString();
+                Alumno a = new Alumno(nombre,apellido,edad);
+                FireBaseDatabase database = FireBaseDataBase.getInstance();
+                DatabaseReference myRef = database.getReference("Alumno");
+                myRef.setValue(a);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/basfirebase/Modelo/Alumno.java
@@ -0,0 +1,16 @@
+package com.example.basfirebase.Modelo;
+
+public class Alumno {
+    public Stirng nombre;
+    public String apellido;
+    public int edad;
+
+    public Alumno() {
+    }
+
+    public Alumno(Stirng nombre, String apellido, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+}
